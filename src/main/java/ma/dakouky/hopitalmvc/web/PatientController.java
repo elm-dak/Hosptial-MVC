a9e163604a99686deec0f5cee77489ec971aa2c2
El Mestapha DAKOUKY <mestafa.wydad66@gmail.com>
2024-04-20 16:12:21 +0000

    add form and edit

--- src/main/java/ma/dakouky/hopitalmvc/web/PatientController.java
+++ src/main/java/ma/dakouky/hopitalmvc/web/PatientController.java
@@ -1,5 +1,6 @@
 package ma.dakouky.hopitalmvc.web;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import ma.dakouky.hopitalmvc.entities.Patient;
 import ma.dakouky.hopitalmvc.repository.PatientRepository;
@@ -7,8 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -37,6 +41,53 @@ public class PatientController {
                          @RequestParam(name = "page",defaultValue = "0") int page){
         patientRepository.deleteById(id);
         return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> patientList(){
+        return patientRepository.findAll();
+    }
+
+    @GetMapping("/formPatients")
+    public String formPatients(Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+
+    }
+
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient,
+                       BindingResult bindingResult,
+                       @RequestParam(defaultValue = "") String keyword,
+                       @RequestParam(defaultValue = "0") int page
+
+        ){
+        if(bindingResult.hasErrors()){
+            return "formPatients";
+        }
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+
+    }
+
+    @GetMapping("/editPatients")
+    public String editPatients(Model model, Long id, String keyword ,int page){
+        Patient patient = patientRepository.findById(id).orElse(null);
+
+            if (patient == null) throw new RuntimeException("Patient introuvable");
+
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+
+        return "editPatients";
+
     }
 
 }
