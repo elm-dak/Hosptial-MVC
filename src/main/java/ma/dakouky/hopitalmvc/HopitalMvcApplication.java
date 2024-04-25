87e02ddd6c1e280fb65a57255ead8fd42963fe40
El Mestapha DAKOUKY <mestafa.wydad66@gmail.com>
2024-04-25 07:36:51 +0000

    add security

--- src/main/java/ma/dakouky/hopitalmvc/HopitalMvcApplication.java
+++ src/main/java/ma/dakouky/hopitalmvc/HopitalMvcApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -54,5 +57,9 @@ public class HopitalMvcApplication implements CommandLineRunner {
 
 
 
+    }
+    @Bean
+    PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
     }
 }
